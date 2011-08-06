1e6770a11a223aca1467a96185b4439470c6c121
Franck Verrot <franck@verrot.fr>
2011-08-06 10:28:44 +0200

    Aliased :say with :text

--- lib/dalek/builtin/say.rb
+++ lib/dalek/builtin/say.rb
@@ -1,10 +1,11 @@
 module Dalek
-	module Builtin
-		module Say
-			def say(what)
-				@room.text(what) {} unless what.empty?
-				what
-			end
-		end
-	end
+  module Builtin
+    module Say
+      def say(what)
+        @room.text(what) {} unless what.empty?
+        what
+      end
+      alias :text :say
+    end
+  end
 end
