77ff9cca1135ab00ab8fe8b507e48c5fae76c306
Franck Verrot <franck@verrot.fr>
2011-08-06 10:52:26 +0200

    WTF is wrong with my settings

--- lib/dalek/builtin/paste.rb
+++ lib/dalek/builtin/paste.rb
@@ -1,10 +1,10 @@
 module Dalek
-	module Builtin
-		module Paste
-			def paste(what)
-				@room.paste(what) {} unless what.empty?
-				what
-			end
-		end
-	end
+  module Builtin
+    module Paste
+      def paste(what)
+        @room.paste(what) {} unless what.empty?
+        what
+      end
+    end
+  end
 end
